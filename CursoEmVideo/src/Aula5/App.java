7cbb081f18b1b73e47c3684600a0104cf53d7d23
eduardaperez <97349220+eduardaperez@users.noreply.github.com>
2024-04-08 21:10:23 -0300

    Aula 5 com criação de classe de conta de banco como projeto de fixação realizado pelo aluno sozinho

---
+++ CursoEmVideo/src/Aula5/App.java
@@ -0,0 +1,17 @@
+package Aula5;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Aula 5");
+
+        ContaBanco conta1 = new ContaBanco(0001, "Maria");
+        conta1.status();
+        conta1.abrirConta("cc");
+        conta1.status();
+        conta1.depositar(10);
+        conta1.fecharConta();
+        conta1.sacar(110);
+        conta1.pagarMensal();
+
+    }
+}
