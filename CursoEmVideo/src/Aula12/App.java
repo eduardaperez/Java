26547999cb2f47106ca93002378bd0842641c393
eduardaperez <97349220+eduardaperez@users.noreply.github.com>
2024-04-22 21:36:21 -0300

    aula 12 - Polimorfismo - parte teórica

---
+++ CursoEmVideo/src/Aula12/App.java
@@ -0,0 +1,17 @@
+package Aula12;
+/* Polimorfismo:
+ * Várias formas de se fazer algo ||
+ * Permite que o mesmo nome represente vários comportamentos diferentes
+ * 
+ * Assinatura do método:
+ * Iguais = mesma quantidade e tipos de PARÂMETROS
+ * Diferentes = parâmetros em quantidade e/ou tipos diferentes
+ * 
+ * Tipos:
+ * Sobreposição = sobrescrever com @override
+ *  -> acontece quando substituimos um método de uma superclasse na sua subclasse, usando a mesma assinatura
+ * Sobrecarga =  
+ */
+public class App {
+    
+}
